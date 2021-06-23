95a2405453eb489f6eefbc51941eb22e4da208b1
Aadityasiva <67770098+aadityasivaS@users.noreply.github.com>
2021-06-23 11:35:57 +0530

    added cube as static

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import "./style.css";
+import "./cube.glb";
 import * as THREE from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
